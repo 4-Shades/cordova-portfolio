363b3d3dc534cb3103f78045d45e4889b6736d3e
Chaim Joseph A. Cordova <chaime123456@gmail.com>
2026-09-18 21:21:32 +0800

    Added picture for wave turbine calculator thumbnail

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -10,16 +10,16 @@ const projectsData: Project[] = [
     title: 'Wave Turbine Power Calculator',
     description: `This application is a small ASP.NET Core MVC web app for estimating
     the power output of a wave turbine system.`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: '/static/images/wave-energy-buoy-corpower-ocean.jpg',
     href: 'https://www.google.com',
   },
-  {
-    title: 'Vertical Wind Turbine Power Calculator',
-    description: `This is a small Spring Boot web application for calculating the 
-    power output of a vertical-axis wind turbine (VAWT).`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
+  // {
+  //   title: 'Vertical Wind Turbine Power Calculator',
+  //   description: `This is a small Spring Boot web application for calculating the 
+  //   power output of a vertical-axis wind turbine (VAWT).`,
+  //   imgSrc: '/static/images/google.png',
+  //   href: 'https://www.google.com',
+  // },
 ]
 
 export default projectsData
