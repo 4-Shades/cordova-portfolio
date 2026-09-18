8f9d73550f629a9959b6146eb70c972524596d2c
Chaim Joseph A. Cordova <chaime123456@gmail.com>
2026-09-18 20:27:24 +0800

    Initial commit

---
+++ data/projectsData.ts
@@ -0,0 +1,25 @@
+interface Project {
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'Wave Turbine Power Calculator',
+    description: `This application is a small ASP.NET Core MVC web app for estimating
+    the power output of a wave turbine system.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'Vertical Wind Turbine Power Calculator',
+    description: `This is a small Spring Boot web application for calculating the 
+    power output of a vertical-axis wind turbine (VAWT).`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+]
+
+export default projectsData
