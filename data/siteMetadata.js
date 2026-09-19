446ff031ec94034834ee676c0076864b504a882c
Chaim Joseph A. Cordova <chaime123456@gmail.com>
2026-09-19 11:19:37 +0800

    Updated author name on footer section

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  author: 'Chaim Joseph Cordova',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
